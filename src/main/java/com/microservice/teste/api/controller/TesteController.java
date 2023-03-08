4b68da508cf2eda3820281baa568979a458cedc1
Thiago Guimarães <thiagobrg98@gmail.com>
2023-03-08 20:35:38 -0300

    comunicacao

--- src/main/java/com/microservice/teste/api/controller/TesteController.java
+++ src/main/java/com/microservice/teste/api/controller/TesteController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 
@@ -19,9 +20,18 @@ public class TesteController {
 	@Value("${microservice.teste}")
 	private String value;
 	
+	@Value("${microservice.remote.name}")
+	private String remoteName;
+	
 	@GetMapping
 	public String teste() {
 		return "Value: " + value;
+	}
+	
+	@GetMapping("/comunicacao")
+	public String comunicacao() {
+		String response = new RestTemplate().getForObject("http://" + remoteName + "/teste", String.class);
+		return "Me comuniquei com: " + response;
 	}
 
 }
