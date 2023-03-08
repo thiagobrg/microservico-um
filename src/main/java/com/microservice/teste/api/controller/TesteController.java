e23da1569ee1263d74b83ff7f6d61bfd66a12ae9
Thiago Guimarães <thiagobrg98@gmail.com>
2023-03-08 20:14:42 -0300

    Teste

--- src/main/java/com/microservice/teste/api/controller/TesteController.java
+++ src/main/java/com/microservice/teste/api/controller/TesteController.java
@@ -1,5 +1,6 @@
 package com.microservice.teste.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(originPatterns = "*", maxAge = 3600)
 public class TesteController {
 	
+	@Value("${microservice.teste}")
+	private String value;
+	
 	@GetMapping
 	public String teste() {
-		return "teste";
+		return "Value: " + value;
 	}
 
 }
